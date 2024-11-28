db6000e3409366dd673bfce8f38804bf1e43fe68
tsubasa.ozawa <t.ozawa.sake@gmail.com>
2024-11-28 22:15:41 +0900

    update:基本設定追加

---
+++ cmd/scaffold.go
@@ -0,0 +1,86 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// scaffoldCmd represents the scaffold command
+var scaffoldCmd = &cobra.Command{
+	Use:   "scaffold [name]",
+	Short: "Generate a new application scaffold",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		appName := args[0]
+		if err := generateScaffold(appName); err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Application scaffold '%s' created successfully!\n", appName)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(scaffoldCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// scaffoldCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// scaffoldCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func generateScaffold(appName string) error {
+	basePath := filepath.Join(".", appName)
+	dirs := []string{
+		filepath.Join(basePath, "cmd"),
+		filepath.Join(basePath, "pkg"),
+		filepath.Join(basePath, "internal"),
+		filepath.Join(basePath, "configs"),
+	}
+
+	files := map[string]string{
+		filepath.Join(basePath, "main.go"): mainTemplate,
+	}
+
+	// Create directories
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+	}
+
+	// Create files
+	for path, content := range files {
+		if err := os.WriteFile(path, []byte(content), 0644); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+const mainTemplate = `package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, {{ .AppName }}!")
+}
+`
